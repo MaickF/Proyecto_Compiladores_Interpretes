cfe682b3581c98a8879bc18d86db9fc193c05882
Fabian <superfabian58@gmail.com>
2023-04-16 00:52:31 -0600

    Primera prueba del lexer

---
+++ ProyectoCompiladores/src/MainProject.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import Proyecto.Lexer;
+import Proyecto.parser;
+import java_cup.internal_error;
+import java_cup.runtime.Symbol;
+
+import Proyecto.*;
+
+public class MainProject {
+    public void initLexerParser(String rutaLexer, String rutaParser) throws internal_error, Exception{
+        generarLexer(rutaLexer);
+        generarParser(rutaParser);
+    }
+
+    public void generarLexer(String rutaLexer) throws internal_error, Exception, IOException{
+        String[] strArr = {rutaLexer};
+        jflex.Main.main(strArr);
+    }
+
+    public void generarParser(String rutaParser) throws internal_error, Exception, IOException{
+        String[] strArr = {rutaParser};
+        java_cup.Main.main(strArr);
+    }
+
+    public void ejercicioLexer(String ruta)throws Exception{
+        System.out.println("Entro");
+        Reader reader = new BufferedReader(new FileReader(ruta));
+        reader.read();
+        Lexer lex = new Lexer(reader);
+        int i = 0;
+        Symbol token;
+        System.out.println("AntesIteracion");
+        while(true){
+            System.out.println("Itera");
+            token = lex.next_token();
+            if(token.sym != 0)
+                System.out.println("Token:" + token.sym + ", valor; " + lex.yytext());
+            else{
+                System.out.println("Cantidad de lexemas encontrados: " +i);
+                return;
+            }
+            i++;
+        }
+    }
+
+    public void ejercicioParser(String ruta)throws Exception{
+        Reader reader = new FileReader(ruta);
+        Lexer lex = new Lexer(reader);
+        parser parser = new parser(lex);
+        parser.parse(); 
+    }
+    
+}
