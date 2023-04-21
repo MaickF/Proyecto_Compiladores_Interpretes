631a2eb9abcc8f52a096dbba03489c85b49c8548
Fabián Villalobos Rodríguez <superfabian58@gmail.com>
2023-04-20 23:46:55 -0600

    Documentación interna

--- src/MainProject.java
+++ src/MainProject.java
@@ -9,21 +9,26 @@ import java_cup.runtime.Symbol;
 import Proyecto.*;
 
 public class MainProject {
+
+
     public void initLexerParser(String rutaLexer, String rutaParser) throws internal_error, Exception{
         generarLexer(rutaLexer);
         generarParser(rutaParser);
     }
 
+    //Recibe la ruta donde se desea generar el lexer y lo genera
     public void generarLexer(String rutaLexer) throws internal_error, Exception, IOException{
         String[] strArr = {rutaLexer};
         jflex.Main.main(strArr);
     }
 
+    //Recibe la ruta donde se desea generar el parser y lo genera
     public void generarParser(String rutaParser) throws internal_error, Exception, IOException{
         String[] strArr = {"-parser", "parser", rutaParser};
         java_cup.Main.main(strArr);
     }
 
+    //Recibe la ruta donde se encuentra el archivo de prueba del lexer y realiza la prueba
     public void ejercicioLexer(String ruta)throws Exception{
         Reader reader = new BufferedReader(new FileReader(ruta));
         reader.read();
@@ -44,6 +49,7 @@ public class MainProject {
         }
     }
 
+    //Recibe la ruta donde se encuentra el archivo de prueba del parser y realiza la prueba
     public void ejercicioParser(String ruta)throws Exception{
         Reader reader = new FileReader(ruta);
         Lexer lex = new Lexer(reader);
