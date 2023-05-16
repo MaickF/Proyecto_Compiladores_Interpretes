875445a39ff40145522cc49b3faa1d981babfcb7
Fabian <superfabian58@gmail.com>
2023-05-16 13:08:12 -0600

    Correccion de errores

--- programa/src/App.java
+++ programa/src/App.java
@@ -32,7 +32,7 @@ public class App {
     }
 
     public static void main(String[] args) throws Exception {
-        //generarParserLexer(); 
-        pruebas();
+        generarParserLexer(); 
+        //pruebas();
     }
 }
